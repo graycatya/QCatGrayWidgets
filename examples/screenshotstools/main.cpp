a09a49c610771f3dc69b5ac4e0f6f4212b3f5ae7
cargray <greycatya@163.com>
2024-04-17 15:34:34 +0800

    feat: 调整放大镜测试

--- examples/screenshotstools/main.cpp
+++ examples/screenshotstools/main.cpp
@@ -4,12 +4,23 @@
 
 int main(int argc, char *argv[])
 {
+
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    QCoreApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Round);
+#endif
+
     QApplication a(argc, argv);
     QCatGrayScreenShotsTools w;
     w.setMasklayerColor(QColor(0, 0, 0, 199));
 
     w.setBorderColor(Qt::red);
-    w.setBorderWidth(5);
+    w.setBorderWidth(20);
     w.setRectRadius(250);
     w.setScreeningMode(QCatGrayScreenShotsTools::MAGNIFYINGGLASS_MODE);
     w.setFilterBoxMovable(true);
