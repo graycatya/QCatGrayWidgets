d8a4afc90f3645e7be414129ee4c20bcdd338e5a
zhouzp <greycatya@163.com>
2024-07-26 09:57:46 +0800

    feat: update quick widget

---
+++ src/QCatGrayQuickWidget/QCatGrayQuickWidget.h
@@ -0,0 +1,43 @@
+#ifndef QCATGRAYQUICKWIDGET_H
+#define QCATGRAYQUICKWIDGET_H
+
+#include <QWidget>
+#include <QtCore/qurl.h>
+
+
+class QQuickView;
+class QQmlEngine;
+
+class QCatGrayQuickWidget : public QWidget
+{
+    Q_OBJECT
+    Q_PROPERTY(ResizeMode resizeMode READ resizeMode WRITE setResizeMode)
+    Q_PROPERTY(Status status READ status NOTIFY statusChanged)
+    Q_PROPERTY(QUrl source READ source WRITE setSource DESIGNABLE true)
+public:
+    QCatGrayQuickWidget(const QUrl &source, QWidget *parent = nullptr);
+    QCatGrayQuickWidget(QQmlEngine *engine, QWidget *parent = nullptr);
+    explicit QCatGrayQuickWidget(QWidget *parent = nullptr);
+
+    virtual ~QCatGrayQuickWidget() override;
+
+
+    enum ResizeMode { SizeViewToRootObject, SizeRootObjectToView };
+    Q_ENUM(ResizeMode)
+    ResizeMode resizeMode() const;
+    void setResizeMode(ResizeMode);
+
+    enum Status { Null, Ready, Loading, Error };
+    Q_ENUM(Status)
+    Status status() const;
+
+    QQuickView* view() const {
+        return m_pView;
+    };
+
+private:
+    QQuickView *m_pView = nullptr;
+
+};
+
+#endif
